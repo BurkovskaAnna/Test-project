2123eeea0ece7b05b90d51c5560011e777ce288f
Anna Burkovska <anuta871@gmail.com>
2017-05-17 18:13:30 +0300

    first

---
+++ js/common.js
@@ -0,0 +1,81 @@
+//Документация: http://fancybox.net/howto
+	//<a class="fancybox"><img src="image.jpg" /></a>
+	//<a class="fancybox" data-fancybox-group="group"><img src="image.jpg" /></a>
+	// $(".fancybox").fancybox();
+
+//Photo-gallery
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active_dot", "");
+  }
+  slides[slideIndex-1].style.display = "inline-block"; 
+  dots[slideIndex-1].className += " active";
+}
+
+
+(function() {
+    if (!!location.hash) return;
+
+    var link = document.querySelector('#tabs > .tab-link');
+    if (link) link.click();
+})();
+
+$('.owl-carousel').owlCarousel({
+    nav: true,
+    navText: ["<img src='img/slider_left_arrow.svg'>","<img src='img/slider_right_arrow.svg'>"],
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:false
+        }
+    }
+})
+ 
+
+function openCity(evt, cityName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("city");
+  for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active-item", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active-item";
+}
+
+
